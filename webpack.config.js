ccacde6c652468b21e65faa95ba5f36575e260c4
spbo <spbo@users.noreply.github.com>
2020-11-02 11:25:19 +0200

    Setup production build and server

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,39 @@
 const path = require('path');
 
-module.exports = {
-  mode: 'development',
-  entry: './src/app.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  }, 
-  module: {
-    rules:[{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-         options: {
-           presets: ['@babel/preset-env', '@babel/preset-react'],
-           plugins: ['@babel/plugin-proposal-class-properties']
+module.exports = (env) => {
+  const isProduction = env === 'production';
+
+  return {
+    mode: isProduction ? 'production' : 'development',
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [{
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            plugins: ['@babel/plugin-proposal-class-properties']
+          }
         }
-      }
-    }, {
+      }, {
         test: /\.s?css$/,
         use: [
           'style-loader',
           'css-loader',
           'sass-loader'
         ]
-    }]
-  },
-  devtool: 'eval-cheap-module-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public')
-  }
+      }]
+    },
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true,
+    }
+  };
 };
