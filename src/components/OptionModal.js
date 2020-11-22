692b91e50a959fa07987042f06a6bf0fc99a2f8f
spbo <64007071+spbo@users.noreply.github.com>
2020-11-22 13:50:15 +0200

    Update OptionModal.js

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import Modal from 'react-modal';
 
-// Accessibility when using Modal
-// Warning: react-modal: App element is not defined.
-// Please use`Modal.setAppElement(el)` or set`appElement={el}`.
-// This is needed so screen readers don't see main content when modal is opened. 
-// It is not recommended, but you can opt - out by setting`ariaHideApp={false}`.
 Modal.setAppElement(document.querySelector('#app'));
 
 const OptionModal = (props) => (
